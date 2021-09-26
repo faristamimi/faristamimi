92c465c528d63c54228c38cbcc3f29d4707ba3a8
faristamimi <90571750+faristamimi@users.noreply.github.com>
2021-09-26 14:56:57 +0400

    Add files via upload

---
+++ pixart.py
@@ -0,0 +1,14 @@
+import turtle
+
+import test
+
+
+def turtle_move(x, y):
+    turtle.goto(50,  80)
+    pass
+
+
+if __name__ == '__main__':
+    x = input("Enter the x coordinates ")
+    y = input('Enter the y coordinates ')
+    turtle_move(x, y)
