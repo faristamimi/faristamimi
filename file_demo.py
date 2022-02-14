630f7410a1223a81453fce4db2bd5b807140d8e5
faristamimi <90571750+faristamimi@users.noreply.github.com>
2022-02-14 08:52:33 +0400

    Add files via upload

--- file_demo.py
+++ file_demo.py
@@ -1,18 +1,20 @@
 def read_csv():
-    with open("data\grades2.csv") as in_file:
+    with open("data\grades.csv") as in_file:
         sum = 0
         count = 0
         next(in_file)
-        # read
         for line in in_file:
             count = count + 1
             line = line.strip()
             tokens = line.split(",")
             final = int(tokens[4])
-            # data proccessing
             sum = sum + final
-            print("The running total is: ", sum)
+    print("count is", count)
+    print("this is the final sum", sum)
+    average = sum / count
+    print("average: ", average)
 
-        print("count is: ", count)
-        print("final sum is: ", sum)
-        average = sum / count
+
+def student_student_average():
+    with open("data\grades.csv") as in_file:
+        return
